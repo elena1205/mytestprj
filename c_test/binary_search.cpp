ce1d00eb34c69957f8fc045f0827cf35698d2cbf
ghostrider <371850480@qq.com>
2017-09-17 22:33:26 +0800

    binary search complete with two ways of iteration and recursivation

--- c_test/binary_search.cpp
+++ c_test/binary_search.cpp
@@ -8,31 +8,59 @@ using namespace std;
 #define TRUE         1
 #define FALSE        0
 
-int binary_search(int tb[], int n, int key)
+int binary_search_iteration(int tb[], int n, int key)
 {
-	int ret = FALSE;
+	int ret = -1;
 	int start = 0;
-	int end = n - 1;
+	int end = n;
+	int pos = 0;
 
-	while(start < end)
+	while((start <= end))
 	{
-		cout<<"start : "<<start<<" end : "<<end<<" cur : "<<tb[(end - start + 1) / 2 + start]<<endl;
+		pos = (end + start ) / 2 ; 
+		// cout<<"start : "<<start<<" end : "<<end<<" cur : "<<tb[pos]<<endl;
 
-		if (tb[(end - start + 1) / 2 + start] == key)
+		if (tb[pos] == key)
 		{
-			ret = TRUE;
+			ret = pos;
 			break;
 		}
-		else if (tb[(end - start + 1) / 2 + start] < key)
+		else if (tb[pos] < key)
 		{
-			start = (end - start + 1) / 2 + start;
+			start = pos + 1;
 		}
 		else
 		{
-			end = (end - start + 1) / 2 + start;
+			end = pos - 1;
 		}	
 	}
 	
+
+	return ret;
+}
+
+int binary_search_recursively(int tb[], int start, int end, int key)
+{
+	int ret = -1;
+	int pos = (start + end) / 2;
+	
+	if (start > end)
+	{
+		return ret;
+	}
+
+	if(tb[pos] == key)
+	{
+		ret = pos;
+	}
+	else if (tb[pos] < key)
+	{
+		ret = binary_search_recursively(tb, pos + 1, end, key);
+	}
+	else
+	{
+		ret = binary_search_recursively(tb, start, pos - 1, key);
+	}
 
 	return ret;
 }
@@ -48,8 +76,19 @@ void test_case(void)
 	}
 	cout<<endl;
 
-	assert(binary_search(numbers, MAX_NUMBERS, 66));
+	assert(-1 == binary_search_iteration(numbers, MAX_NUMBERS, -1));
+	assert(-1 == binary_search_iteration(numbers, MAX_NUMBERS, 66));
+	assert(34 == binary_search_iteration(numbers, MAX_NUMBERS, 69));
+	assert(13 == binary_search_iteration(numbers, MAX_NUMBERS, 27));
+	assert(55 == binary_search_iteration(numbers, MAX_NUMBERS, 111));
 
+	assert(44 == binary_search_recursively(numbers, 0, MAX_NUMBERS-1, 89));
+	assert(25 == binary_search_recursively(numbers, 0, MAX_NUMBERS-1, 51));
+	assert(61 == binary_search_recursively(numbers, 0, MAX_NUMBERS-1, 123));
+	assert(8 == binary_search_recursively(numbers, 0, MAX_NUMBERS-1, 17));
+	assert(-1 == binary_search_recursively(numbers, 0, MAX_NUMBERS-1, 150));
+
+	cout<<"test passed!"<<endl;
 }
 
 int main(int argc, char const *argv[])
